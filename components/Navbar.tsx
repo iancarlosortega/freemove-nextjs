73d9271b64a7ede3dc0086f19097ba0a2d9a716c
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2023-06-08 10:16:58 -0500

    Navbar phone with sidebar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,33 @@
 'use client';
 
+import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
+import {
+	Drawer,
+	List,
+	ListItem,
+	ListItemButton,
+	ListItemIcon,
+	ListItemText,
+} from '@mui/material';
+import {
+	Apartment,
+	Menu,
+	HealthAndSafety,
+	MiscellaneousServices,
+	AccountTree,
+} from '@mui/icons-material';
 import styles from './Navbar.module.css';
-import { TextField } from '@mui/material';
 
 export const Navbar = () => {
 	const pathname = usePathname();
 	const style = {
-		backgroundColor: pathname === '/' ? 'black' : 'var(--dark-blue)',
+		backgroundColor: pathname === '/' ? 'transparent' : 'var(--dark-blue)',
 	};
+
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
 
 	return (
 		<>
@@ -25,16 +42,16 @@ export const Navbar = () => {
 				</Link>
 				<ul className={styles.list}>
 					<li className={styles.listItem}>
-						<a href='#city'>Ciudad</a>
+						<a href='#ciudad'>Ciudad</a>
 					</li>
 					<li className={styles.listItem}>
-						<a href='#health'>Salud</a>
+						<a href='#salud'>Salud</a>
 					</li>
 					<li className={styles.listItem}>
-						<a href='#services'>Servicios</a>
+						<a href='#servicios'>Servicios</a>
 					</li>
 					<li className={styles.listItem}>
-						<a href='#process'>Proceso</a>
+						<a href='#proceso'>Proceso</a>
 					</li>
 					<li>
 						<Link href='/dashboard' className={styles.btnDashboard}>
@@ -44,16 +61,92 @@ export const Navbar = () => {
 				</ul>
 			</nav>
 
-			<TextField id='filled-basic' label='Filled' variant='filled' />
-
-			<p>
-				Reprehenderit minim id dolor id eu ad anim non sint nulla nulla est
-				consectetur. Sit sint culpa pariatur non. Lorem excepteur deserunt enim
-				ipsum laborum eu aliquip ea. Ad ullamco labore tempor ea qui id quis
-				labore commodo duis anim. Cupidatat Lorem ad tempor ullamco.
-			</p>
-
-			{/* TODO: Navbar Phone */}
+			<nav className={styles.navbarPhone}>
+				<ul>
+					<li>
+						<Link href='/'>
+							<Image
+								src='/assets/logo-white.png'
+								width={170}
+								height={120}
+								alt='FreeMove Logo'
+							/>
+						</Link>
+					</li>
+					<li>
+						<button onClick={() => setIsMenuOpen(true)}>
+							<Menu />
+						</button>
+					</li>
+				</ul>
+			</nav>
+			<Drawer
+				onBackdropClick={() => setIsMenuOpen(false)}
+				open={isMenuOpen}
+				anchor='left'
+				sx={{
+					backdropFilter: 'blur(4px)',
+					transition: 'all 0.5s ease-out',
+				}}>
+				<List>
+					<ListItem>
+						<Link href='/'>
+							<Image
+								src='/assets/logo-v2.png'
+								width={200}
+								height={90}
+								alt='FreeMove Logo'
+							/>
+						</Link>
+					</ListItem>
+					<ListItem>
+						<ListItemButton onClick={() => setIsMenuOpen(false)} href='#ciudad'>
+							<ListItemIcon>
+								<Apartment />
+							</ListItemIcon>
+							<ListItemText primary='Ciudad' />
+						</ListItemButton>
+					</ListItem>
+					<ListItem>
+						<ListItemButton onClick={() => setIsMenuOpen(false)} href='#salud'>
+							<ListItemIcon>
+								<HealthAndSafety />
+							</ListItemIcon>
+							<ListItemText primary='Salud' />
+						</ListItemButton>
+					</ListItem>
+					<ListItem>
+						<ListItemButton
+							onClick={() => setIsMenuOpen(false)}
+							href='#servicios'>
+							<ListItemIcon>
+								<MiscellaneousServices />
+							</ListItemIcon>
+							<ListItemText primary='Servicios' />
+						</ListItemButton>
+					</ListItem>
+					<ListItem>
+						<ListItemButton
+							onClick={() => setIsMenuOpen(false)}
+							href='#proceso'>
+							<ListItemIcon>
+								<AccountTree />
+							</ListItemIcon>
+							<ListItemText primary='Proceso' />
+						</ListItemButton>
+					</ListItem>
+					<ListItem>
+						<Link
+							href='/dashboard'
+							className={styles.btnDashboard}
+							style={{
+								marginTop: '1rem',
+							}}>
+							Registro/Ingreso
+						</Link>
+					</ListItem>
+				</List>
+			</Drawer>
 		</>
 	);
 };
